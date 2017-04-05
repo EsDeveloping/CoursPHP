afbfb951332b5e98ffcedc32ea2769d1515176ac
EsDeveloping <esfordeveloping@gmail.com>
2017-04-05 16:08:09 +0200

    Correction du lien vers le tp page protege

--- index.php
+++ index.php
@@ -21,6 +21,40 @@
 				<p>PHP est un langage serveur il permet donc de dinamiser sont site, le contenu ne sera donc plus fixe il poura s'adapter a l'utilisateur ou même a vos envis <br>
 				MySql est une bases de donnée, ça vous sera très utile pour enregistrer les données de vos visiteurs des historique etc...</p>
 			</article>
+			<article>
+				<h1>utilisation d'emmet</h1>
+
+				li>li.item-$>a
+
+				<li>
+					<li class="item-1"><a href=""></a></li>
+				</li>
+
+				li>li#item-$>a
+
+				<li>
+					<li id="item-1"><a href=""></a></li>
+				</li>
+				
+				lundi
+				mardi
+				mercredi
+				jeudi
+				vendredi
+				samedi
+				dimanche
+				
+				<p>Shift + click droit pour mettre des multi curseur</p>
+				<p>shift + control + g pour entourer la selection d'un element</p>
+
+				<li class="day-01"><span>Lundi</span></li>
+				<li class="day-02"><span>Lundi</span></li>
+				<li class="day-03"><span>Lundi</span></li>
+				<li class="day-04"><span>Lundi</span></li>
+				<li class="day-05"><span>Lundi</span></li>
+				<li class="day-06"><span>Lundi</span></li>
+				<li class="day-07"><span>Lundi</span></li>
+			</article>
 		</section>
 	</section>
 </body>
