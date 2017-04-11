e07a6f2bb3ae1feb3a9bae2c8367b9e0351f257c
EsDeveloping <esfordeveloping@gmail.com>
2017-04-11 09:28:38 +0200

    Modit du mdp sur le vaio

--- blog2.php
+++ blog2.php
@@ -16,7 +16,7 @@
 			
 			<?php 
 				$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-
+	
 				$requete = $bdd->query('SELECT * FROM blogBillet');
 
 
--- sql/page1.php
+++ sql/page1.php
@@ -1,6 +1,6 @@
 <?php
 	// Connection
-	$bdd = new PDO('mysql:host=localhost;dbname=test', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	$bdd = new PDO('mysql:host=localhost;dbname=test', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 	// Requete SQL
 
